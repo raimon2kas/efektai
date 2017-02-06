809ed5bb3917c01758ca09f40658215bfb1f5632
KamiZut <kzutautaite@gmail.com>
2017-02-06 20:11:42 +0200

    .

--- js/article.js
+++ js/article.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('article').hide().delay(1000).fadeIn(2200);
-    $('div.hidden').fadeIn(1000).removeClass('hidden').css({"left":"500px"}).animate({"left":"0px"}, "slow");
+    $('div.hidden').fadeIn(1000).removeClass('hidden');
 
 });
