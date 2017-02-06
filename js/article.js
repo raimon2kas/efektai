02ace9b1e0e2940d320414efe6cf47366fbeb3e8
raimon2kas <rrimce221@yahoo.com>
2017-02-06 18:37:45 +0200

    Istrinti index failai

--- js/article.js
+++ js/article.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
     $('article').hide().delay(1000).fadeIn(2200);
-    $('div.hidden').fadeIn(1000).removeClass('hidden');
+    $('div.hidden').fadeIn(1000).removeClass('hidden').css({"left":"500px"}).animate({"left":"0px"}, "slow");
+
 });
